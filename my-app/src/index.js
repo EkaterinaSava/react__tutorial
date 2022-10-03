81254077d7057c71733ab757650ff51b708cc7d6
Ekaterina Kozlova <e.kozlova@devcaz.com>
2022-10-03 17:46:30 +0200

    Refactor component Game - add histrory

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -38,21 +38,51 @@ function Board(props) {
 }
 
 function Game() {
-  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [history, setHistory] = useState([{
+    squares: Array(9).fill(null)
+  }]);
+  const [stepNumber, setStepNumber] = useState(0);
   const [xIsNext, setXIsNext] = useState(true);
 
   const handleClick = (i) => {
+    const historyUpdated = history.slice(0, stepNumber + 1);
+    const current = historyUpdated[historyUpdated.length - 1];
+    const squares = current.squares.slice();
+
     if ( calculateWinner(squares) || squares[i] ) {
       return;
     }
 
     squares[i] = xIsNext ? 'X' : 'O';
 
-    setSquares(squares);
+    setHistory(historyUpdated.concat([
+      {
+        squares: squares
+      }
+    ]));
+    setStepNumber(historyUpdated.length);
     setXIsNext(!xIsNext);
   }
 
-  const winner = calculateWinner(squares);
+  const jumpTo = (step) => {
+    setStepNumber(step);
+    setXIsNext((step % 2) === 0);
+  }
+
+  const current = history[stepNumber];
+  const winner = calculateWinner(current.squares);
+
+  const moves = history.map((step, move) => {
+    const desc = move ?
+      'Go to move #' + move :
+      'Go to game start!';
+
+    return (
+      <li key={move}>
+          <button onClick={() => jumpTo(move)}>{desc}</button>
+      </li>
+    );
+  });
 
   let status;
   if (winner) {
@@ -66,13 +96,14 @@ function Game() {
 
       <div className="game-board">
         <Board
-          squares={squares}
+          squares={current.squares}
           onClick={(i) => handleClick(i)}
         />
       </div>
 
       <div className="game-info">
         <div>{status}</div>
+        <ol>{moves}</ol>
       </div>
 
     </div>
